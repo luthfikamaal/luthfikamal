ca3177549c7a00e36574db8fb36bc2c2a672a7e5
luthfikamaal <luthfikamal13@gmail.com>
2022-09-12 09:38:19 +0700

    change color

--- src/views/post/ShowPost.vue
+++ src/views/post/ShowPost.vue
@@ -3,13 +3,13 @@
     <div class="h-screen rounded-md bg-indigo-100"></div>
   </div>
   <div class="show-post">
-    <div class="mb-3 rounded-md bg-indigo-700 p-2 text-center">
+    <div class="mb-3 rounded-md bg-blue-700 p-2 text-center">
       <h1 class="font-poppins mb-1 text-2xl text-white">{{ post.title }}</h1>
       <p v-show="post.title != null" class="font-ssp text-center text-slate-100">By Muhammad Luthfi Kamal</p>
     </div>
     <div v-html="post.text" class="post-content font-serif"></div>
     <div class="my-3">
-      <span class="my-3 rounded-full bg-indigo-500 px-3 py-1 text-white">{{ category.name }}</span>
+      <span class="my-3 rounded-full bg-blue-500 px-3 py-1 text-white">{{ category.name }}</span>
     </div>
   </div>
 </template>
