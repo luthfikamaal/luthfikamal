3117c5c7f66d012730731ee23460e875a208ce14
Muhammad Luthfi Kamal <90312645+luthfikamaal@users.noreply.github.com>
2022-10-08 07:51:02 +0700

    Update ShowPost.vue

--- src/views/post/ShowPost.vue
+++ src/views/post/ShowPost.vue
@@ -4,8 +4,7 @@
   </div>
   <div class="show-post">
     <div class="mb-3 rounded-md bg-blue-700 p-2 text-center">
-      <h1 class="font-poppins mb-1 text-2xl text-white">{{ post.title }}</h1>
-      <p v-show="post.title != null" class="font-ssp text-center text-slate-100">By Muhammad Luthfi Kamal</p>
+      <h1 class="font-poppins text-2xl text-white">{{ post.title }}</h1>
     </div>
     <div v-html="post.text" class="post-content font-serif"></div>
     <div class="my-3">
